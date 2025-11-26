45c6efd81e9aff660b41172525b346661cd4b526
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-11-26 09:10:37 +0200

    Izstrādāti pirmie trīs case

---
+++ src/saraksts/Uzd1.java
@@ -0,0 +1,76 @@
+package saraksts;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
+public class Uzd1 {
+	static boolean jauEksiste(LinkedList<String> saraksts, String elements) {
+		for(int i=0;i<saraksts.size(); i++) {
+			if(saraksts.get(i).equalsIgnoreCase(elements)) {
+				JOptionPane.showMessageDialog(null, "Šads produkts jau ir pievienots sarakstam", "Kļuda",
+						JOptionPane.WARNING_MESSAGE);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String izvele, koPievienot, koAtrast, koNonemt, arKoAizstat;
+		int kurPievienot, kurNonemt, kuruMainit;
+		LinkedList<String> saraksts = new LinkedList<>();
+		
+		do {
+			do {
+			izvele = JOptionPane.showInputDialog("1 - Produktu\n2 - Produktu skaits\n"+
+			"3 - Izvadīt produktus\n4 - Atrast produktu\n"+ 
+			"5 - Pievienot Konkrētā pozīcija\n"+
+			"6 - Noņemt produktu\n7 - Noņemt pēc indeksa\n"+
+			"8 - Noņemt pirmo \n9 - Noņemt pēdējo\n"+
+			"10 - Mainīt produktu\n11 - Sakārtot alfabeētiski\n"+
+			"12 - Nodzēst sarakstu\n0 - Apturēt");	
+			
+			if(izvele == null) 
+				izvele = "0";
+			}while(!izvele.matches("\\d+"));
+			
+			switch(izvele) {
+			case "1":			
+			do {
+				koPievienot =
+				JOptionPane.showInputDialog("Kādu produktu pievienot?");
+			}while((jauEksiste(saraksts, koPievienot) == true)||
+			!koPievienot.matches("[\\p{L}&&\\p{IsLatin}]+"));
+			
+			saraksts.add(koPievienot.toLowerCase());
+			JOptionPane.showMessageDialog(null, "Produkts pievienots!", "Informācija",
+					JOptionPane.WARNING_MESSAGE);
+				break;
+			
+			case "2":
+				JOptionPane.showMessageDialog(null, "Produktu sakaits: "+
+						saraksts.size(), "Informācija", JOptionPane.INFORMATION_MESSAGE);
+				break;
+				
+			case "3":
+				if(saraksts.size() == 0)
+					JOptionPane.showMessageDialog(null, "Nav sarakstā produktu", 
+							"Informācija", JOptionPane.INFORMATION_MESSAGE);
+				
+				else {
+					Iterator<String> izvade = saraksts.iterator();
+					String str = "";
+					while(izvade.hasNext()) {
+					str += izvade.next()+"\n";
+				}
+				JOptionPane.showMessageDialog(null, str, "Produktu saraksts", JOptionPane.INFORMATION_MESSAGE);
+				}
+				break;
+			}
+		}while(!izvele.equals("0"));
+		
+		
+		}
+}
